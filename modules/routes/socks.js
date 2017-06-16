3cb5e03c52088b1274d6fb6c6486c2eab570668f
Brett Miller <miller.brett.andrew@gmail.com>
2017-06-16 10:48:38 -0500

    added http get

---
+++ modules/routes/socks.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+
+
+
+router.get('/', function(req, res) {
+  console.log('socks get route hit');
+  res.send('argyle');
+}); //end get
+
+module.exports = router;
--- public/scripts/sockdrawer.js
+++ public/scripts/sockdrawer.js
@@ -1,8 +1,9 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('SockController', function() {
+myApp.controller('SockController', function(SockService) {
   var vm = this;
   vm.getSocks = function() {
     console.log('in getSocks');
+    SockService.checkSocks();
   };
 });
